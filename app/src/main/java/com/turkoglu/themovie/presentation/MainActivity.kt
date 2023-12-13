e58eda30b2cdafdf7659c3400b7848b4d34daec9
Salih TÜRKOĞLU <77458433+SalihT95@users.noreply.github.com>
2023-12-13 19:14:48 +0300

    update

--- app/src/main/java/com/turkoglu/themovie/presentation/MainActivity.kt
+++ app/src/main/java/com/turkoglu/themovie/presentation/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.turkoglu.themovie.presentation
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -35,6 +37,7 @@ data class BottomNavigationItem(
 @OptIn(ExperimentalMaterial3Api::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -44,7 +47,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //MovieApp()
+                    MovieApp()
                 }
             }
         }
--- app/src/main/java/com/turkoglu/themovie/util/Constants.kt
+++ app/src/main/java/com/turkoglu/themovie/util/Constants.kt
@@ -4,7 +4,7 @@ package com.turkoglu.themovie.util
 // API Token : eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDI5OGI1M2ZlMWIyYjBhOWIyNWQ0MTVmNzQyYWNiNSIsInN1YiI6IjY1NWI3MzRjYjU0MDAyMTRkM2NjMmViNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.n9oeea5w_wstakN7sZ8Mq854ufNtiIi0JRqGB0kViSI
 
 object Constants {
-    const val BASE_URL = "https://api.themoviedb.org/3"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
     const val API_KEY = "a0298b53fe1b2b0a9b25d415f742acb5"
     const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
 }
